094b54d9d199e2ee73b852df58a81a119b69b3bc
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-24 09:32:27 +0300

    next task corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 function formatPhones(tel) {
   let formattedTel = tel;
 
-  const addPlus = new RegExp("^([0-9])");
-  const leaveDashesAndStaples = new RegExp("([- _()])", "g");
+  // const addPlus = new RegExp('^([0-9])');
+  // const leaveDashesAndStaples = new RegExp('([- _()])', 'g');
+  // const replaceFirstNumbers = new RegExp('^\+89');
 
-  formattedTel = formattedTel.replace(addPlus, "+$1");
-  formattedTel = formattedTel.replace(leaveDashesAndStaples, "");
-  return formattedTel.replace(/^\+89/, "+79");
+  formattedTel = formattedTel.replace(/^([0-9])/, '+$1');
+  formattedTel = formattedTel.replace(/([- _()])/g, '');
+  return formattedTel.replace(/^\+89/, '+79');
 }
 
 export default formatPhones;
